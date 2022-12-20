453a2be50ae771e7f4e9cc3abd3271e4eeaca878
Yusuf Arslan <yusufarsln98@outlook.com>
2022-12-20 19:10:05 +0300

    Update index.js

--- components/index.js
+++ components/index.js
@@ -4,4 +4,3 @@ export { default as HeaderTab } from './Header/HeaderTab/HeaderTab';
 export { default as Tabs } from './Tabs/Tabs';
 export { default as CommunityProfilePage } from './ComunityProfilePage/CommunityProfilePage';
 export { default as Post } from './Post/Post';
-//test
