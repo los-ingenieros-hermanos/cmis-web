5f16cc3f9e00d052ce24fdbaf7119afa83cf5b5c
samildeli <samildeliogullari@gmail.com>
2022-12-31 11:29:55 +0300

    Update authContext

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,27 @@
-import '../styles/globals.scss';
-import Head from 'next/head';
 import { Header } from 'components';
-import { createContext, useCallback, useState } from 'react';
+import Head from 'next/head';
+import { createContext, useCallback, useEffect, useState } from 'react';
+import '../styles/globals.scss';
 
-export function api(path) {
+function api(path) {
   //return 'https://cmisservice-cmis-backend.azuremicroservices.io/api/' + path;
   return 'http://localhost:8070/api/' + path;
+}
+
+async function request(method, path, body, useCredentials) {
+  const res = await fetch(api(path), {
+    method,
+    credentials: useCredentials && 'include',
+    body,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  let data;
+  if (res.ok) {
+    data = await res.json();
+  }
+  return [res, data];
 }
 
 export const AuthContext = createContext();
@@ -15,34 +31,33 @@ function MyApp({ Component, pageProps }) {
   const [isSignUpOpen, setIsSignUpOpen] = useState(false);
   const [userData, setUserData] = useState();
 
+  useEffect(() => {
+    const userData_ = JSON.parse(localStorage.getItem('userData'));
+    setUserData(userData_);
+  }, []);
+
+  function setUserData_(data) {
+    setUserData(data);
+    localStorage.setItem('userData', JSON.stringify(data));
+  }
+
   const signIn = useCallback(async (email, password) => {
-    const res = await fetch(api('auth/signin'), {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ email, password }),
-    });
+    const [res, data] = await request('POST', 'auth/signin', JSON.stringify({ email, password }), true);
     if (res.ok) {
-      const data = await res.json();
-      setUserData(data);
+      setUserData_(data);
       return data;
     }
   }, []);
 
   const signUp = useCallback(
     async (firstName, lastName, email, password, role) => {
-      const res = await fetch(api('auth/signup'), {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ firstName, lastName, email, password, role: [role] }),
-      });
+      const [res, data] = await request(
+        'POST',
+        'auth/signup',
+        JSON.stringify({ firstName, lastName, email, password, role: [role] }),
+      );
       if (res.ok) {
         await signIn(email, password);
-        const data = await res.json();
         return data;
       }
     },
@@ -50,61 +65,53 @@ function MyApp({ Component, pageProps }) {
   );
 
   const signOut = useCallback(async () => {
-    const res = await fetch(api('auth/signout'), {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+    const [res, _] = await request('POST', 'auth/signout', undefined, true);
     if (res.ok) {
-      setUserData(undefined);
+      setUserData_(null);
     }
   }, []);
 
   const getCommunities = useCallback(async () => {
-    const res = await fetch(api(`cmis/communities`), {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    if (res.ok) {
-      const data = await res.json();
-      return data;
-    }
+    const [_, data] = await request('GET', 'cmis/communities');
+    return data;
   }, []);
 
   const getFollowedCommunities = useCallback(async () => {
-    const res =
-      userData?.roles[0] === 'ROLE_STUDENT' &&
-      (await fetch(api(`cmis/students/${userData.id}/followingCommunities`), {
-        method: 'GET',
-        credentials: 'include',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }));
-    if (res?.ok) {
-      const data = await res.json();
-      console.log(data);
+    if (userData?.roles[0] === 'ROLE_STUDENT') {
+      const [_, data] = await request('GET', `cmis/students/${userData.id}/followingCommunities`, undefined, true);
       return data;
     }
   }, [userData]);
 
-  const updateCommunityData = useCallback(async (communityData) => {
-    const res = await fetch(api(`cmis/communities/${communityData.id}`), {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(communityData),
-    });
-    if (res.ok) {
-      const data = await res.json();
-      return data;
-    }
+  const getCommunity = useCallback(async (communityId) => {
+    const [_, data] = await request('GET', `cmis/communities/${communityId}`, undefined, true);
+    return data;
   }, []);
+
+  const updateCommunity = useCallback(async (communityData) => {
+    const [_, data] = request('PUT', `cmis/communities/${communityData.id}`, JSON.stringify(communityData));
+    return data;
+  }, []);
+
+  const followCommunity = useCallback(
+    async (communityId) => {
+      if (userData?.roles[0] === 'ROLE_STUDENT') {
+        const [_, data] = request('POST', `cmis/students/${userData.id}/followingCommunities`, { id: communityId });
+        return data;
+      }
+    },
+    [userData],
+  );
+
+  const unfollowCommunity = useCallback(
+    async (communityId) => {
+      if (userData?.roles[0] === 'ROLE_STUDENT') {
+        const [_, data] = request('DELETE', `cmis/students/${userData.id}/followingCommunities${communityId}`);
+        return data;
+      }
+    },
+    [userData],
+  );
 
   return (
     <AuthContext.Provider
@@ -117,9 +124,12 @@ function MyApp({ Component, pageProps }) {
         signIn,
         signOut,
         userData,
-        updateCommunityData,
         getCommunities,
         getFollowedCommunities,
+        getCommunityData: getCommunity,
+        updateCommunityData: updateCommunity,
+        followCommunity,
+        unfollowCommunity,
       }}
     >
       <Head>
