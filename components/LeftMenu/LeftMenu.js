f77510c1f3b890e72de27ce46bda20bc34328b21
Yusuf Arslan <yusufarsln98@outlook.com>
2023-01-03 21:44:18 +0300

    Update LeftMenu.js
    fixing some errors

--- components/LeftMenu/LeftMenu.js
+++ components/LeftMenu/LeftMenu.js
@@ -1,15 +1,16 @@
 import styles from "./LeftMenu.module.scss";
 import Link from "next/link";
+import { AuthContext } from 'pages/_app';
+import React from 'react';
 
-export default function LeftMenu({ authContext }) {
+export default function LeftMenu() {
+    const authContext = React.useContext(AuthContext);
     return (
         <ul className={styles.left}>
-            {authContext.signIn ? (
-                <li className={styles.item}>
-                  <Link href={'/profilim'}>
-                      <img src={'/icons/sidebar-sign-in.svg'} alt='profilim' />
-                  </Link>
-                  <Link href={'/profilim'}>Profilim</Link>
+            {authContext.userData ? (
+                <li className={styles.item} onClick={() => authContext.signOut()}>
+                  <img src={'/icons/sidebar-sign-in.svg'} alt='cikis' style={{transform: 'rotate(180deg)'}} />
+                  <a>Çıkış Yap</a>
                 </li>
             ) : (
                 <li className={styles.item} onClick={() => authContext.setIsLoginOpen(true)}>
@@ -17,12 +18,12 @@ export default function LeftMenu({ authContext }) {
                   <a>Giriş Yap</a>
                 </li>
             )}
-            {authContext.signIn ? (
+            {authContext.userData ? (
                 <li className={styles.item}>
-                  <Link href={'/kaydedilenler'}>
+                  <Link href={'/ogrenciler/' + authContext.userData.id + "/kaydedilenler"}>
                       <img src={'/icons/sidebar-bookmark.svg'} alt='kaydedilenler' />
                   </Link>
-                  <Link href={'/profilim'}>Kaydedilenler</Link>
+                  <Link href={'/ogrenciler/' + authContext.userData.id + "/kaydedilenler"}>Kaydedilenler</Link>
                 </li>
             ) : (
                 <li className={styles.item} onClick={() => authContext.setIsSignUpOpen(true)}>
