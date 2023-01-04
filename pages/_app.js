d6c810b8c4a75b0b963daa22730a5b5b582c4a7b
samildeli <samildeliogullari@gmail.com>
2023-01-04 13:28:01 +0300

    Add new api requests and add pfp to header

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,15 @@ import { createContext, useCallback, useEffect, useState } from 'react';
 import '../styles/globals.scss';
 
 function api(path) {
-  // return 'https://cmisbackend.azurewebsites.net/api/' + path;
+  //return 'https://cmisbackend.azurewebsites.net/api/' + path;
   return 'http://localhost:8070/api/' + path;
 }
 
 async function request(method, path, body, useCredentials) {
+  if (!path) {
+    return [null, null];
+  }
+
   const res = await fetch(api(path), {
     method,
     credentials: useCredentials && 'include',
@@ -32,18 +36,35 @@ function MyApp({ Component, pageProps }) {
   const [userData, setUserData] = useState();
 
   useEffect(() => {
-    const userData_ = JSON.parse(localStorage.getItem('userData'));
-    setUserData(userData_);
+    function getUserData() {
+      const userData_ = JSON.parse(localStorage.getItem('userData'));
+      if (userData_ && !isSessionExpired(userData_)) {
+        setUserData_(userData_);
+      } else {
+        setUserData(undefined);
+      }
+    }
+
+    getUserData();
+    setInterval(getUserData, 9000);
   }, []);
 
   function setUserData_(data) {
+    if (data) {
+      data.isCommunity = data.roles[0] === 'ROLE_COMMUNITY' || data.roles[0] === 'ROLE_UNVERIFIED';
+    }
     setUserData(data);
     localStorage.setItem('userData', JSON.stringify(data));
+  }
+
+  function isSessionExpired(userData_) {
+    return parseInt(userData_.expires) - 10000 < new Date().getTime();
   }
 
   const signIn = useCallback(async (email, password) => {
     const [res, data] = await request('POST', 'auth/signin', JSON.stringify({ email, password }), true);
     if (res.ok) {
+      data.expires = new Date().getTime() + 86400000;
       setUserData_(data);
       return data;
     }
@@ -89,29 +110,117 @@ function MyApp({ Component, pageProps }) {
   }, []);
 
   const updateCommunity = useCallback(async (communityData) => {
-    const [_, data] = request('PUT', `cmis/communities/${communityData.id}`, JSON.stringify(communityData));
+    const [_, data] = await request('PUT', `cmis/communities/${communityData.id}`, JSON.stringify(communityData));
     return data;
   }, []);
 
   const followCommunity = useCallback(
     async (communityId) => {
-      if (userData?.roles[0] === 'ROLE_STUDENT') {
-        const [_, data] = request('POST', `cmis/students/${userData.id}/followingCommunities`, { id: communityId });
-        return data;
-      }
+      const [_, data] = await request('POST', `cmis/students/${userData.id}/followingCommunities`, {
+        id: communityId,
+      });
+      return data;
     },
     [userData],
   );
 
   const unfollowCommunity = useCallback(
     async (communityId) => {
+      const [_, data] = await request('DELETE', `cmis/students/${userData.id}/followingCommunities/${communityId}`);
+      return data;
+    },
+    [userData],
+  );
+
+  const applyToCommunity = useCallback(
+    async (communityId, description) => {
+      const [_, data] = await request('POST', `cmis/communities/${communityId}/apply/${userData.id}`, description);
+      return data;
+    },
+    [userData],
+  );
+
+  const leaveCommunity = useCallback(
+    async (communityId) => {
+      const [_, data] = await request('DELETE', `cmis/communities/${communityId}/members/${userData.id}`);
+      return data;
+    },
+    [userData],
+  );
+
+  const getMembers = useCallback(async () => {
+    const [_, data] = await request(`cmis/communities/${userData.id}/members`);
+    return data;
+  }, [userData]);
+
+  const getMemberApplications = useCallback(async () => {
+    const [_, data] = await request(`cmis/communities/${userData.id}/membersApplications`);
+    return data;
+  }, [userData]);
+
+  const getMemberApplication = useCallback(async (communityId, studentId) => {
+    const [_, data] = await request(`cmis/communities/${communityId}/membersApplications/${studentId}`);
+    return data;
+  }, []);
+
+  const cancelMemberApplication = useCallback(
+    async (communityId) => {
+      const [_, data] = await request('DELETE', `cmis/communities/${communityId}/cancelApplication/${userData.id}`);
+      return data;
+    },
+    [userData],
+  );
+
+  const acceptMemberApplication = useCallback(
+    async (studentId) => {
+      const [_, data] = await request('PUT', `cmis/communities/${userData.id}/membersApplications/${studentId}/accept`);
+      return data;
+    },
+    [userData],
+  );
+
+  const rejectMemberApplication = useCallback(
+    async (studentId) => {
+      const [_, data] = await request('PUT', `cmis/communities/${userData.id}/membersApplications/${studentId}/reject`);
+      return data;
+    },
+    [userData],
+  );
+
+  const isFollowerOfCommunity = useCallback(
+    async (communityId) => {
       if (userData?.roles[0] === 'ROLE_STUDENT') {
-        const [_, data] = request('DELETE', `cmis/students/${userData.id}/followingCommunities${communityId}`);
+        const [_, data] = await request(`cmis/students/${userData.id}/isFollowerOf/${communityId}`);
         return data;
       }
     },
     [userData],
   );
+
+  const isMemberOfCommunity = useCallback(
+    async (communityId) => {
+      if (userData?.roles[0] === 'ROLE_STUDENT') {
+        const [_, data] = await request(`cmis/students/${userData.id}/isMemberOf/${communityId}`);
+        return data;
+      }
+    },
+    [userData],
+  );
+
+  const getCommunityPosts = useCallback(async (communityId) => {
+    const [res, data] = await request(`cmis/communities/${communityId}/posts`);
+    return data;
+  }, []);
+
+  const getUserPfp = useCallback(async () => {
+    let _, data;
+    if (userData?.roles[0] === 'ROLE_STUDENT') {
+      [_, data] = await request(`cmis/students/${userData.id}/image`);
+    } else if (userData?.roles[0] === 'ROLE_COMMUNITY') {
+      [_, data] = await request(`cmis/communities/${userData.id}/image`);
+    }
+    return data;
+  }, [userData]);
 
   return (
     <AuthContext.Provider
@@ -126,10 +235,22 @@ function MyApp({ Component, pageProps }) {
         userData,
         getCommunities,
         getFollowedCommunities,
-        getCommunityData: getCommunity,
-        updateCommunityData: updateCommunity,
+        getCommunity,
+        updateCommunity,
         followCommunity,
         unfollowCommunity,
+        isFollowerOfCommunity,
+        isMemberOfCommunity,
+        applyToCommunity,
+        leaveCommunity,
+        getMembers,
+        getMemberApplications,
+        getMemberApplication,
+        cancelMemberApplication,
+        acceptMemberApplication,
+        rejectMemberApplication,
+        getCommunityPosts,
+        getUserPfp,
       }}
     >
       <Head>
